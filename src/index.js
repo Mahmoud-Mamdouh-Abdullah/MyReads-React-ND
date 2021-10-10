ca983f4832d504c73c87bea7035cc247be01e305
Mahmoud Mamdouh Abd Allah <49378579+Mahmoud-Mamdouh-Abdullah@users.noreply.github.com>
2021-10-10 20:36:13 +0200

    final commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { HashRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <HashRouter basename='/'>
+  <HashRouter>
     <App />
   </HashRouter>,
   document.getElementById('root')
